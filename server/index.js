2c8be743fac099e0afd1916e46509fd677cf2483
vince <edil3ra@gmail.com>
2017-09-14 12:52:20 +0200

    Upload is checking if the filename is an image

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ var utils = require('./utils')
 
 
 var DB_NAME = 'db.json'
-var COLLECTION_NAME = 'images'
+var COLLECTION_IMAGE = 'images'
 var UPLOAD_PATH = 'uploads'
-var upload = multer({ dest: `${UPLOAD_PATH}/` })
+var upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: utils.imageFilter })
 var db = new Loki(`${DB_NAME}`, { persistenceMethod: 'fs' })
 
 
@@ -19,17 +19,54 @@ var db = new Loki(`${DB_NAME}`, { persistenceMethod: 'fs' })
 var app = express()
 
 
-app.post('/images', upload.single('image'), function(req, res) {
-    utils.loadCollection(COLLECTION_NAME, db).then(function(collection){
-		var data = collection.insert(req.file)
-		db.saveDatabase()
+// app.get('/images', function (req, res) => {
+//     try {
+//         const col = await loadCollection(COLLECTION_IMAGE, db);
+//         res.send(col.data);
+//     } catch (err) {
+//         res.sendStatus(400);
+//     }
+// })
+
+
+
+// app.post('/images', upload.single('image'), function(req, res) {
+// 	try {
+// 		console.log('on try')
+// 		utils.loadCollection(COLLECTION_IMAGE, db).then(function(collection){
+// 			var data = collection.insert(req.file)
+// 			console.log('i before after save database')
+// 			db.saveDatabase()
+// 			console.log('i passe after save database')
+// 			res.send({ id: data.$loki, fileName: data.filename, originalName: data.originalname })
+// 		})
+// 	} catch(err) {
+// 		console.log('on catch')
+// 		res.sendStatus(400)
+// 	}
+// })
+
+
+app.post('/images', function(req, res) {
+	upload.single('image')(req, res, function(err) {
+		if(err) {
+			return res.sendStatus(400)
+			
+		}
 		try {
-			res.send({ id: data.$loki, fileName: data.filename, originalName: data.originalname })
+			utils.loadCollection(COLLECTION_IMAGE, db).then(function(collection){
+				var data = collection.insert(req.file)
+				db.saveDatabase()
+				res.send({ id: data.$loki, fileName: data.filename, originalName: data.originalname })
+			})
 		} catch(err) {
 			res.sendStatus(400)
-		}
+		}		
 	})
 })
+
+
+
 
 
 
--- server/utils.js
+++ server/utils.js
@@ -2,13 +2,26 @@ var del = require('del')
 var Loki = require('lokijs')
 
 
-const loadCollection = function (colName, db) {
+var loadCollection = function (colName, db) {
     return new Promise(resolve => {
         db.loadDatabase({}, () => {
-            const _collection = db.getCollection(colName) || db.addCollection(colName)
+            var _collection = db.getCollection(colName) || db.addCollection(colName)
             resolve(_collection);
         })
     })
 }
 
-module.exports.loadCollection = loadCollection
+
+var imageFilter = function (req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('Only image files are allowed!'), false)
+    }
+    cb(null, true)
+}
+
+
+module.exports = {
+	loadCollection: loadCollection,
+	imageFilter: imageFilter,
+}
+
